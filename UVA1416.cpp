f3c34610c9f34fc2bd1fc48dab9326289516508b
kristeski <kiko.rici.2007@gmail.com>
2025-09-20 15:20:14 +0300

    Средено Малце

--- UVA1416.cpp
+++ UVA1416.cpp
@@ -2,21 +2,41 @@
 using namespace std;
 
 // Решение со Флојд Варшалов временска комплексност O(M * V^3)
-
+#define int long long
 int v, m, l; 
-const int N = 101;
+const int N = 101, inf = 1 << 29;
 
 struct Edge {
     int u, v, w;
 };
+int floyd(const vector<Edge> &vec, const Edge ed) {
+    int res = 0;
+    vector<vector<int>> dp(v + 1, vector<int>(v + 1, inf));
+    for(int i = 1; i <= v; i++)
+        dp[i][i] = 0;
+    for(Edge e: vec) {
+        if(e.u == ed.u && e.v == ed.v && e.w == ed.w) 
+            continue;
+        dp[e.u][e.v] = e.w;
+        dp[e.v][e.u] = e.w;
+    }
 
-int main() {
+    for(int k = 1; k <= v; k++) 
+        for(int i = 1; i <= v; i++) 
+            for(int j = 1; j <= v; j++)
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+    
+    for(int i = 1; i <= v; i++) 
+        for(int j = 1; j <= v; j++)
+            res += (dp[i][j] < inf) ? dp[i][j] : l;
+    return res;
+}
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     vector<Edge> edgelist;
     edgelist.reserve(10 * N);
-    edgelist.push_back({0, 0, 1 << 29});
 
     cin >> v >> m >> l;
 
@@ -26,34 +46,12 @@ int main() {
         edgelist.push_back({a, b, w});
     }
 
-    int maxi = 0, res, original;
-    bool first = false;
+    int maxi = 0, res, original = 0;
+    original = floyd(edgelist, {0, 0, 0});
 
-    for (Edge cur : edgelist) { 
-        vector<vector<int>> dp(v + 1, vector<int>(v + 1, l));
-        for (int i = 1; i <= v; i++)
-            dp[i][i] = 0;
-        for (Edge e1 : edgelist) {
-            if (cur.u == e1.u && cur.v == e1.v && cur.w == e1.w) continue;
-            dp[e1.u][e1.v] = e1.w;
-            dp[e1.v][e1.u] = e1.w;
-        }
-        res = 0;
-        for (int k = 1; k <= v; k++)
-            for (int i = 1; i <= v; i++)
-                for (int j = 1; j <= v; j++)
-                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
-
-        for (int i = 1; i <= v; i++) {
-            for (int j = 1; j <= v; j++)
-                res += dp[i][j];
-        }
-        maxi = max(maxi, res);
-        if (!first) {
-            original = maxi;
-            first = true;
-        }
-    }
+    for(Edge e : edgelist) 
+        maxi = max(maxi, floyd(edgelist, e));
+    
     cout << original << " " << maxi;
     return 0;
 }
